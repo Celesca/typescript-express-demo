047aa62165959be42ab7a9a53f387c2007c3ef0c
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-03 15:38:13 +0700

    fix: database.ts

--- core/src/database.ts
+++ core/src/database.ts
@@ -1,11 +1,34 @@
-
+import path from 'path';
+import fs from 'fs/promises';
+import fsSync from 'fs';
 
 const databasePrefix = 'data';
 
-export class Database {
-    // Short form constructor
-    constructor(private collectionName: string
-                ,protected options: object) {}
+export interface DatabaseOptions {
+    defaultData?: object; // Optional Field
 }
 
-const database = new Database('users',  { max: 1000 });
+export class Database {
+    private databasePath: string;
+    // Short form constructor
+    constructor(private collectionName: string,protected options? : DatabaseOptions ) {
+        this.databasePath = path.join(databasePrefix, collectionName + '.json');
+    }
+
+    async init() {
+        const defaultData = this.options?.defaultData ?? [];
+        if (!fsSync.existsSync(databasePrefix)) {
+            await fs.mkdir(databasePrefix, { recursive: true });
+            await fs.writeFile(
+                this.databasePath,
+                JSON.stringify(defaultData, null, 2),
+                'utf-8'
+            );
+        
+        }
+
+        return this;
+    }
+}
+
+const database = new Database('users',  { defaultData: { name: 'John'} });
